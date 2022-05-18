4ddd77a2bb175a7fe0fa40c4556df7ec258ccb7e
Rajesh Pillai <rajesh.algorisys@ext.idfy.com>
2022-05-18 21:54:04 +0530

    XMLNode and doc parsing - 1

--- lxml.h
+++ lxml.h
@@ -14,14 +14,41 @@
 #endif
 
 // Definitions
+struct _XMLNode {
+  char* tag;
+  char* inner_text;
+  struct _XMLNode* parent;
+};
+
+typedef struct _XMLNode XMLNode;
+
+XMLNode* XMLNode_new(XMLNode* parent);
+void XMLNode_free(XMLNode* parent);
+
 struct _XMLDocument {
-  char* source;
+  XMLNode* root;
 };
 
 typedef struct _XMLDocument XMLDocument;
 
 int XMLDocument_load(XMLDocument* doc, const char* path);
 void XMLDocument_free(XMLDocument* doc);
+
+XMLNode* XMLNode_new(XMLNode* parent) {
+  XMLNode* node = (XMLNode*)malloc(sizeof(XMLNode));
+  node->parent = parent;
+  node->tag = NULL;
+  node->inner_text = NULL;
+  return node;
+}
+
+void XMLNode_free(XMLNode* node) {
+  if(node->tag)
+    free(node->tag);
+
+  if(node->inner_text)
+    free(node->inner_text);
+}
 
 // Implementations
 int XMLDocument_load(XMLDocument* doc, const char* path) {
@@ -35,16 +62,45 @@ int XMLDocument_load(XMLDocument* doc, const char* path) {
   fseek(file, 0, SEEK_SET);
 
   // +1 extra byte for non terminating character
-  doc->source = (char*) malloc(sizeof(char) * size + 1);
+  char* buf = (char*) malloc(sizeof(char) * size + 1);
 
-  fread(doc->source, 1, size, file);
+  fread(buf, 1, size, file);
   fclose(file);
-  doc->source[size] = '\0';
+  buf[size] = '\0';
+
+  doc->root = XMLNode_new(NULL); // root node -> parent is null
+  char lex[256];
+  int lexi = 0;
+  int i = 0;
+
+  XMLNode* curr_node = NULL;
+
+  while(buf[i] != '\0') {
+    if(buf[i] == '<') {
+      if(!curr_node)
+        curr_node = doc->root;
+      else 
+        curr_node = XMLNode_new(curr_node);
+
+      i++;
+      while(buf[i] != '>') {
+        lex[lexi++] = buf[i++];
+      }
+      lex[lexi] = '\0';
+      curr_node->tag = strdup(lex);
+      lexi = 0;
+      i++;
+      continue;
+    } else {
+      lex[lexi++] = buf[i++];
+    }
+  }
+
   return TRUE;
 }
 
 void XMLDocument_free(XMLDocument* doc) {
-  free(doc->source);
+  XMLNode_free(doc->root);
 }
 
 #endif  // LITTLE_XML_H
--- test.c
+++ test.c
@@ -3,7 +3,7 @@
 int main() {
   XMLDocument doc;
   if (XMLDocument_load(&doc, "test.xml")) {
-    printf("%s\n", doc.source);
+    printf("%s\n", doc.root->tag);
     XMLDocument_free(&doc);
   }
   return 0;
